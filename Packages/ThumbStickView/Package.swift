e05bf1aa54a803e2e8134424d584b42b4c5fb289
Jiwon Park <156501481+jiwonpark831@users.noreply.github.com>
2025-07-15 20:48:32 +0900

    fix: ThumbStickView 패키지 swift-tools 버전 수정

--- Packages/ThumbStickView/Package.swift
+++ Packages/ThumbStickView/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 /*
  See the LICENSE.txt file for this sample’s licensing information.
