c18dc71f4b668162a7bd2e0d8a15c1925bcbc5d6
Hyeongi Kim (Wade) <21900215@handong.ac.kr>
2025-07-28 23:58:02 +0900

    fix: JoyStickButtonView UI 조정

--- TtouchIsland/Sources/Game/GameView.swift
+++ TtouchIsland/Sources/Game/GameView.swift
@@ -14,13 +14,11 @@ struct GameView: View {
     @State private var currentScale: CGFloat = 1.0
     @State private var lastScale: CGFloat = 1.0
 
-    @State var showInterface: Bool = false
     @State private var showResetAlert = false
 
     var body: some View {
         ZStack {
-            Color.black
-                .ignoresSafeArea()
+            Color.black.ignoresSafeArea()
 
             RealityView { content in
                 guard
@@ -28,39 +26,33 @@ struct GameView: View {
                         named: "Scene",
                         in: dummyAssetsBundle
                     )
-                else { return }
+                else {
+                    print("‼️ 에셋 로드 실패: Scene")
+                    return
+                }
 
                 manager.gameRoot = game
 
                 await initializeGameSetting(game, content)
                 content.add(game)
+
                 print("게임 세팅 완료")
 
                 DispatchQueue.main.async { manager.isGameReady = true }
-
-                showInterface = true
             }
-            .ignoresSafeArea()
-            .zIndex(0)
-            // id가 변경되면 뷰를 새로 그림
             .id(gameId)
 
-            if showInterface {
-                if !manager.isFocusedOnItem {
-                    GameStatusView()
-                        .padding(.top, 26)
-                } else {
-                    GameStatusView()
-                }
+            // MARK: - Interface View
 
-                PlatformerThumbControl(
+            if manager.isGameReady {
+                JoystickButtonView(
                     manager: manager,
                     itemAction: { item, camera in
                         if item.components[ItemComponent.self]?.type
                             == .newspaper
                         {
                             print("📰")
-                            handleNewspaperItem(item: item, camera: camera)
+                            ItemManager().handleNewspaperItem(item: item, camera: camera)
                         }
                         if manager.visibleItems.count == 1 {
                             if item.components[ItemComponent.self]?.type
@@ -86,7 +78,8 @@ struct GameView: View {
                                 // 땃쥐 행복해하는 로티 애니메이션 플레이
                                 manager.updateStatus(to: .getItem)
 
-                                Task { await setCharacterScaleUp() }
+                                Task { await ItemManager().setCharacterScaleUp() }
+
                                 manager.visibleItems[1].isSolid = true
                                 item.removeFromParent()
                                 manager.nearItem = nil
@@ -98,6 +91,8 @@ struct GameView: View {
 
                                 // 땃쥐 행복해하는 로티 애니메이션 플레이
                                 manager.updateStatus(to: .getItem)
+
+                                ItemManager().setCharacterRunButtonAvailable()
 
                                 manager.visibleItems[2].isSolid = true
                                 item.removeFromParent()
@@ -133,12 +128,12 @@ struct GameView: View {
                                 manager.nearItem = nil
                             }
                         }
-                    },
+                    }
                 )
-                .zIndex(1)
             }
+
             // 초기화 버튼
-            // TO DO: UI 변경
+            // TODO: UI 변경
             VStack {
                 HStack {
                     Spacer()
@@ -155,7 +150,6 @@ struct GameView: View {
             Button("네", role: .confirm) {
                 manager.isGameReady = false
                 manager.resetGame()
-                showInterface = false
                 // 새로운 게임 아이디를 설정해줘서 realityview를 다시 그리게 한다
                 gameId = UUID()
             }
@@ -221,137 +215,8 @@ struct GameView: View {
             )
         }
     }
-
-    fileprivate struct PlatformerThumbControl: View {
-        let manager: GameManager
-        let itemAction: (_ item: Entity, _ camera: Entity) -> Void
-
-        @State var characterJoystick: CGPoint = .zero
-        @State var cameraAngleThumbstick: CGPoint = .zero
-
-        var body: some View {
-            VStack {
-                if manager.isFocusedOnItem {
-                    HStack {
-                        Spacer()
-
-                        Button(action: {
-                            // 뒤로가기 액션 호출
-                            if let item = manager.nearItem,
-                                let camera = manager.gameCamera
-                            {
-                                itemAction(item, camera)
-                            }
-                        }) {
-                            Image(systemName: "xmark")
-                                .frame(width: 36, height: 36)
-                                .foregroundColor(.black)
-                                .font(.system(size: 24))
-                                .glassEffect(.regular.interactive())
-                        }
-                        .padding()
-                    }
-                }
-
-                Spacer()
-
-                if !manager.isFocusedOnItem {
-                    HStack(alignment: .bottom) {
-                        ThumbStickView(updatingValue: $characterJoystick)
-                            .onChange(of: characterJoystick) { _, newValue in
-                                let movementVector: SIMD3<Float> =
-                                    [Float(newValue.x), 0, Float(newValue.y)]
-                                        / 10
-                                manager.character?
-                                    .components[
-                                        CharacterMovementComponent.self
-                                    ]?
-                                    .controllerDirection = movementVector
-                            }
-                        Spacer()
-
-                        ZStack(alignment: .bottomTrailing) {
-                            CameraThumbStickView(
-                                updatingValue: $cameraAngleThumbstick
-                            )
-                            .onChange(of: cameraAngleThumbstick) {
-                                _,
-                                newValue in
-                                let movementVector: SIMD2<Float> =
-                                    [Float(newValue.x), Float(-newValue.y)] / 30
-
-                                manager.gameRoot?.findEntity(named: "camera")?
-                                    .components[WorldCameraComponent.self]?
-                                    .updateWith(
-                                        continuousMotion: movementVector
-                                    )
-                            }
-                            .background(Color.clear)
-
-                            VStack(alignment: .trailing) {
-                                ActionButton(name: "RunIcon")
-                                    .onLongPressGesture(
-                                        minimumDuration: 0.0, // 즉시 반응
-                                        pressing: { isPressed in
-                                            if isPressed {
-                                                manager.updateStatus(to: .run)
-                                                manager.setCharacterRunning(to: true)
-
-                                            } else {
-                                                manager.updateStatus(to: .common)
-                                                manager.setCharacterRunning(to: false)
-                                            }
-                                        },
-                                        perform: {}
-                                    )
-                                    .padding(.bottom, 4)
-
-                                HStack {
-                                    if manager.nearItem != nil {
-                                        Button {
-                                            if let item = manager.nearItem,
-                                               let camera = manager.gameCamera,
-                                               let character = manager.character
-                                            {
-                                                itemAction(item, camera)
-                                                AudioManager.playGetItemSound(
-                                                    root: character
-                                                )
-                                            }
-
-                                        } label: {
-                                            ActionButton(name: "GetIcon")
-                                        }
-                                        .padding(.trailing, 16)
-                                    }
-
-                                    // Jump button.
-                                    ActionButton(name: "JumpIcon")
-                                        .onLongPressGesture(
-                                            minimumDuration: 0.0,
-                                            pressing: { isPressed in
-                                                manager.character?.components[
-                                                    CharacterMovementComponent.self
-                                                ]?.jumpPressed = isPressed
-                                                AudioManager.playJumpSound(
-                                                    root: manager.character!
-                                                )
-                                                manager.updateStatus(to: .jump)
-                                            },
-                                            perform: {}
-                                        )
-                                }
-                            }
-                            .padding()
-                        }
-                    }
-                    .padding(.bottom, 24)
-                }
-            }
-        }
-    }
 }
-//
-//#Preview {
-//    GameView()
-//}
+
+#Preview {
+    GameView()
+}
